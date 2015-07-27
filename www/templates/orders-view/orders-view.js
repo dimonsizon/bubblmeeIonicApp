37a76207657e7c92e743793c6d045fed8035bb82
Sizon Dmitry <dimonsizon@mail.ru>
2015-07-27 14:59:05 +0300

    Added error function

--- www/templates/orders-view/orders-view.js
+++ www/templates/orders-view/orders-view.js
@@ -16,14 +16,17 @@
     function ($scope, $stateParams, $http, $filter) {
         $scope.orders = [];
         $scope.getOrders = function() {
-            $http.get('https://api.bubblmee.com/merchant/pos/a19ed734-947c-4b4a-8fa8-1296e0fc9fc9/orderReport?from=1407618000000&to=1437685200000').success(function (data) {
-                
+            $http.get('https://api.bubblmee.com/merchant/pos/a19ed734-947c-4b4a-8fa8-1296e0fc9fc9/orderReport?from=1407013200000&to=1438030800000').success(function (data) {
                 if (data.orders) {
                     for (var i = 0; i < data.orders.length; ++i) {
                         data.orders[i].timestampModel = $filter('date')(data.orders[i].timestamp, 'd MMMM yyyy ');
                     }
                 }
+                
+                $scope.error = "No Error";
                 $scope.orders = data;
+            }).error(function () {
+                $scope.error = "Error";
             });
         }
 
